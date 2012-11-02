c513bf01a72a9b251949abd4fcd7646c78ebb366
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-01 20:49:07 -0700

    add method for overriding path for servlet request

--- clustermate-service/src/main/java/com/fasterxml/clustermate/service/servlet/ServletServiceRequest.java
+++ clustermate-service/src/main/java/com/fasterxml/clustermate/service/servlet/ServletServiceRequest.java
@@ -17,6 +17,11 @@ public class ServletServiceRequest extends ServiceRequest
      */
     protected final HttpServletRequest _request;
 
+    /**
+     * Path override assigned by {@link #setPath}, if any.
+     */
+    protected String _pathOverride;
+    
     /*
     /**********************************************************************
     /* Life-cycle
@@ -41,6 +46,9 @@ public class ServletServiceRequest extends ServiceRequest
     
     @Override
     public String getPath() {
+        if (_pathOverride != null) {
+            return _pathOverride;
+        }
         return _request.getPathInfo();
     }
 
@@ -61,7 +69,18 @@ public class ServletServiceRequest extends ServiceRequest
     /**********************************************************************
      */
 
+    /**
+     * Accessor for getting underlying {@link HttpServletRequest}
+     */
     public HttpServletRequest getNativeRequest() {
         return _request;
+    }
+
+    /**
+     * Method for overriding path that is returned via {@link #getPath} (and
+     * originally comes from HttpServletRequest#getPathInfo}.
+     */
+    public void setPath(String path) {
+        _pathOverride = path;
     }
 }
