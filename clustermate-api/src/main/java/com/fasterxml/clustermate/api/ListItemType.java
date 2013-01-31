7e6cd02565245bfb67a84a3baecd30f900668937
Tatu Saloranta <tsaloranta@gmail.com>
2013-01-31 13:20:20 -0800

    add new method in ListItemType

--- clustermate-api/src/main/java/com/fasterxml/clustermate/api/ListItemType.java
+++ clustermate-api/src/main/java/com/fasterxml/clustermate/api/ListItemType.java
@@ -2,28 +2,45 @@ package com.fasterxml.clustermate.api;
 
 import java.util.*;
 
+import com.fasterxml.clustermate.api.msg.ListItem;
+import com.fasterxml.storemate.shared.StorableKey;
+
 /**
  * Enumeration of types of list items available for listing entries.
  */
 public enum ListItemType
 {
     /**
-     * Entries are ids in their binary form encoded as Base64 in JSON (and raw bytes in Smile)
+     * Entries are ids in their binary form encoded as Base64 in JSON (and raw bytes in Smile).
+     * Return type will be <link>StorableKey</link>
      */
-    ids,
+    ids(StorableKey.class),
     
     /**
      * Entries are textual representations of ids: may or may not be convertible back
      * to ids, depending on how ids are converted.
+     * Return type will be {@link java.lang.String}
      */
-    names,
+    names(String.class),
     
     /**
      * Full object entries that include id (in binary form) as well as additional
      * StoreMate-level metadata.
      */
-    entries;
+    entries(ListItem.class);
 
+    private final Class<?> _valueType;
+    
+    private ListItemType(Class<?> valueType) {
+        _valueType = valueType;
+    }
+
+    /**
+     * Accessor for getting {@link Class} of actual items returned with this
+     * logical type.
+     */
+    public Class<?> getValueType() { return _valueType; }
+    
     private final static HashMap<String,ListItemType> _entries = new HashMap<String,ListItemType>();
     static {
         for (ListItemType t : ListItemType.values()) {
