504189c6d647a2f0ce08d807271a6d86374afcf6
Tatu Saloranta <tsaloranta@gmail.com>
2013-05-21 22:19:16 -0700

    Refactoring to simplify RequestPathStrategy handling

--- clustermate-api/src/main/java/com/fasterxml/clustermate/api/RequestPathStrategy.java
+++ clustermate-api/src/main/java/com/fasterxml/clustermate/api/RequestPathStrategy.java
@@ -11,38 +11,19 @@ public abstract class RequestPathStrategy
     /* Methods for building requests paths (by client or server-as-client)
     /**********************************************************************
      */
-    
+
     /**
-     * Method for creating the path for accessing stored entries,
-     * but without including actual entry id, given a builder that
-     * refers to the server node to access
+     * Method to call to append path segments of specified type, onto
+     * partially built base path.
      * 
-     * @param nodeRoot Reference to root part of the store node
+     * @param basePath Existing path, on to which append specified path segment(s)
      * 
-     * @return Path for accessing stored entries, not including the actual
-     *    entry id.
+     * @return Path builder after appending specified path
+     * 
+     * @since 0.9.7
      */
-    public abstract <B extends RequestPathBuilder> B appendStoreEntryPath(B nodeRoot);
-
-    public abstract <B extends RequestPathBuilder> B appendStoreListPath(B nodeRoot);
-
-    public abstract <B extends RequestPathBuilder> B appendStoreStatusPath(B nodeRoot);
-    
-    public abstract <B extends RequestPathBuilder> B appendStoreFindEntryPath(B nodeRoot);
-
-    public abstract <B extends RequestPathBuilder> B appendStoreFindListPath(B nodeRoot);
-    
-    // // Node status, related:
-    
-    public abstract <B extends RequestPathBuilder> B appendNodeStatusPath(B nodeRoot);
-
-    public abstract <B extends RequestPathBuilder> B appendNodeMetricsPath(B nodeRoot);
-    
-    // // Sync handling:
-    
-    public abstract <B extends RequestPathBuilder> B appendSyncListPath(B nodeRoot);
-
-    public abstract <B extends RequestPathBuilder> B appendSyncPullPath(B nodeRoot);
+    public abstract <B extends RequestPathBuilder> B appendPath(B basePath,
+            PathType type);
 
     /*
     /**********************************************************************
@@ -55,4 +36,51 @@ public abstract class RequestPathStrategy
      * is a match, consuming matched path.
      */
     public abstract PathType matchPath(DecodableRequestPath pathDecoder);
+
+    /*
+    /**********************************************************************
+    /* Methods for building requests paths (by client or server-as-client)
+    /**********************************************************************
+     */
+
+    /**
+     * Method for creating the path for accessing stored entries,
+     * but without including actual entry id, given a builder that
+     * refers to the server node to access
+     * 
+     * @param nodeRoot Reference to root part of the store node
+     * 
+     * @return Path for accessing stored entries, not including the actual
+     *    entry id.
+     */
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendStoreEntryPath(B nodeRoot) { return null; }
+
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendStoreListPath(B nodeRoot) { return null; }
+
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendStoreStatusPath(B nodeRoot) { return null; }
+    
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendStoreFindEntryPath(B nodeRoot) { return null; }
+
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendStoreFindListPath(B nodeRoot) { return null; }
+    
+    // // Node status, related:
+    
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendNodeStatusPath(B nodeRoot) { return null; }
+
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendNodeMetricsPath(B nodeRoot) { return null; }
+    
+    // // Sync handling:
+    
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendSyncListPath(B nodeRoot) { return null; }
+
+    @Deprecated
+    public <B extends RequestPathBuilder> B appendSyncPullPath(B nodeRoot) { return null; }
 }
