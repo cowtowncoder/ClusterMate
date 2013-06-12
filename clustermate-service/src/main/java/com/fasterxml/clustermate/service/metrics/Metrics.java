0f2073f879862a7109a176f494b0d8438b21f250
Tatu Saloranta <tsaloranta@gmail.com>
2013-06-11 21:48:45 -0700

    Add work-arounds for probs with metrics, tests

--- clustermate-service/src/main/java/com/fasterxml/clustermate/service/metrics/Metrics.java
+++ clustermate-service/src/main/java/com/fasterxml/clustermate/service/metrics/Metrics.java
@@ -1,13 +1,18 @@
 package com.fasterxml.clustermate.service.metrics;
 
+import java.util.HashMap;
+
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.ExponentiallyDecayingReservoir;
 import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 
 /**
  * Simple container for Metrics 3.0 registry.
+ * Bit complicated to work around issues with unit tests, where metrics are registered
+ * multiple types.
  *<p>
  * May need to consider making injectable (or passed via contexts).
  */
@@ -15,16 +20,35 @@ public class Metrics
 {
     protected final static MetricRegistry _metrics = new MetricRegistry();
 
-    public static Counter newCounter(String name) {
-        return _metrics.counter(name);
+    protected final static HashMap<String, Metric> _registered = new HashMap<String,Metric>();
+    
+    public static synchronized Counter newCounter(String name) {
+        Counter c = (Counter) _registered.get(name);
+        if (c == null) {
+            c = _metrics.counter(name);
+            _registered.put(name,  c);
+        }
+        return c;
     }
 
-    public static Timer newTimer(String name) {
-        return _metrics.timer(name);
+    public static synchronized Timer newTimer(String name)
+    {
+        Timer t = (Timer) _registered.get(name);
+        if (t == null) {
+            t = _metrics.timer(name);
+            _registered.put(name, t);
+        }
+        return t;
     }
 
-    public static Histogram newHistogram(String name) {
-        // default would be ExpDecayingReservoir; but let's make explicit
-        return _metrics.register(name, new Histogram(new ExponentiallyDecayingReservoir()));
+    public static synchronized Histogram newHistogram(String name)
+    {
+        Histogram h = (Histogram) _registered.get(name);
+        if (h == null) {
+            // default would be ExpDecayingReservoir; but let's make explicit
+            h = _metrics.register(name, new Histogram(new ExponentiallyDecayingReservoir()));
+            _registered.put(name,  h);
+        }
+        return h;
     }
 }
