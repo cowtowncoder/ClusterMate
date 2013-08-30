05a1efe0925ec5044382286f5db221b69c31a79d
Tatu <tsaloranta@gmail.com>
2013-08-30 13:01:52 -0700

    Minor refactoring to allow redirection of logging

--- clustermate-service/src/main/java/com/fasterxml/clustermate/service/cleanup/FileCleaner.java
+++ clustermate-service/src/main/java/com/fasterxml/clustermate/service/cleanup/FileCleaner.java
@@ -21,7 +21,7 @@ import com.fasterxml.storemate.store.file.*;
  */
 public class FileCleaner extends CleanupTask<FileCleanupStats>
 {
-    private final static Logger LOG = LoggerFactory.getLogger(FileCleaner.class);
+    private final Logger LOG;
     
     protected FileManager _fileManager;
 
@@ -34,8 +34,14 @@ public class FileCleaner extends CleanupTask<FileCleanupStats>
      */
     protected long _maxTimeToLiveMsecs;
     
-    public FileCleaner() { }
+    public FileCleaner() {
+        this(LoggerFactory.getLogger(FileCleaner.class));
+    }
 
+    public FileCleaner(Logger log) {
+        LOG = log;
+    }
+    
     @Override
     protected void init(SharedServiceStuff stuff,
             Stores<?,?> stores,
@@ -48,28 +54,25 @@ public class FileCleaner extends CleanupTask<FileCleanupStats>
         _maxTimeToLiveMsecs = stuff.getServiceConfig().cfgMaxMaxTTL.getMillis()
                 + new TimeSpan("1d").getMillis();
     }
-
+    
     @Override
     public FileCleanupStats _cleanUp()
     {
         final FileCleanupStats stats = new FileCleanupStats();
-
-        LOG.info("Starting file cleanup: will nuke any dirs older than {}",
-                TimeMaster.timeDesc(_maxTimeToLiveMsecs));
+        _reportStart(_maxTimeToLiveMsecs);
         
         // iterate over all but the last directory; last considered current
         List<DirByDate> dateDirs = _fileManager.listMainDataDirs(stats);
         final int dirCount = dateDirs.size();
         if (dirCount == 0) { // none?
-            LOG.warn("No date directories found for clean up -- bailing out");
+            _reportEndNoDirs();
             return stats;
         }
         for (int i = 0, end = dirCount-1; i < end; ++i) {
             _cleanDateDir(dateDirs.get(i), stats);
         }
         // all done
-        LOG.info("Completed file clean up (skipped the last date-dir, '{}'): {}",
-                dateDirs.get(dirCount-1).getDirectory().getAbsolutePath(), stats);
+        _reportEndSuccess(stats, dateDirs.get(dirCount-1).getDirectory());
         return stats;
     }
 
@@ -119,5 +122,33 @@ public class FileCleaner extends CleanupTask<FileCleanupStats>
             LOG.warn("Failed to nuke directory {} for some reason, must skip", dir);
         }
         stats.addRemainingDir();
+    }
+
+    /*
+    /**********************************************************************
+    /* Overridable reporting methods
+    /**********************************************************************
+     */
+
+    protected void _reportStart(long maxTimeToLiveMsecs)
+    {
+        if (LOG != null) {
+            LOG.info("Starting file cleanup: will nuke any dirs older than {}",
+                    TimeMaster.timeDesc(_maxTimeToLiveMsecs));
+        }
+    }
+
+    protected void _reportEndNoDirs() {
+        if (LOG != null) {
+            LOG.warn("No date directories found for clean up -- bailing out");
+        }
+    }
+
+    protected void _reportEndSuccess(FileCleanupStats stats, File skippedDir)
+    {
+        if (LOG != null) {
+            LOG.info("Completed file clean up (skipped the last date-dir, '{}'): {}",
+                skippedDir.getAbsolutePath(), stats);
+        }
     }
 }
