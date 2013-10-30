52c6dbfffec11bbe6aa6ecd16f082a55ed1b3c7f
Tatu <tsaloranta@gmail.com>
2013-10-30 14:22:35 -0700

    More testing to ensure that caller can prevent use of compression

--- clustermate-dropwizard/src/test/java/com/fasterxml/clustermate/jaxrs/common/CompressionTestBase.java
+++ clustermate-dropwizard/src/test/java/com/fasterxml/clustermate/jaxrs/common/CompressionTestBase.java
@@ -28,11 +28,11 @@ public abstract class CompressionTestBase extends JaxrsStoreTestBase
     }
 
     protected abstract String testPrefix();
-    
+
     public void testPreCompressedLZF() throws Exception
     {
         final TimeMasterForSimpleTesting timeMaster = new TimeMasterForSimpleTesting(1234L);
-          
+
         StoreResource<TestKey, StoredEntry<TestKey>> resource = createResource(testPrefix(), timeMaster, true);
 
         // ensure it'll use gzip compression
@@ -101,5 +101,67 @@ public abstract class CompressionTestBase extends JaxrsStoreTestBase
         entries.stop();
     }
 
-    // !!! TODO: No compression tests
+    public void testExplicitDoNotCompress() throws Exception
+    {
+        final TimeMasterForSimpleTesting timeMaster = new TimeMasterForSimpleTesting(1234L);
+        
+        StoreResource<TestKey, StoredEntry<TestKey>> resource = createResource(testPrefix(), timeMaster, true);
+
+        final String INPUT_STR = biggerCompressibleData(45000);
+        final byte[] INPUT_STR_BYTES = INPUT_STR.getBytes("UTF-8");
+        int origSize = INPUT_STR_BYTES.length;
+        
+        // ok: assume empty Entity Store
+        StorableStore entries = resource.getStores().getEntryStore();
+        assertEquals(0, entryCount(entries));
+
+        final TestKey INTERNAL_KEY1 = contentKey(CLIENT_ID,  "data/comp-none/1");
+
+        // then try adding said entry
+        FakeHttpRequest req = new FakeHttpRequest()
+            .addHeader(ClusterMateConstants.HTTP_HEADER_COMPRESSION,
+                Compression.NONE.asContentEncoding());
+        FakeHttpResponse response = new FakeHttpResponse();
+
+        resource.getHandler().putEntry(req, response,
+                INTERNAL_KEY1, calcChecksum(INPUT_STR_BYTES), new ByteArrayInputStream(INPUT_STR_BYTES),
+                null, null, null);
+        if (200 != response.getStatus()) {
+            PutResponse<?> putResponse = response.getEntity();
+            fail("Response ("+response.getStatus()+"), message: "+putResponse.message);
+        }
+        // verify we got a file...
+        assertSame(PutResponse.class, response.getEntity().getClass());
+        PutResponse<?> presp = (PutResponse<?>) response.getEntity();
+
+        assertFalse(presp.inlined);
+
+        // can we count on this getting updated? Seems to be, FWIW
+        assertEquals(1, entryCount(entries));
+
+        // first, verify store has it:
+        StoredEntry<TestKey> entry = rawToEntry(entries.findEntry(StoreOperationSource.REQUEST,
+                null, INTERNAL_KEY1.asStorableKey()));
+        assertNotNull(entry);
+        assertTrue(entry.hasExternalData());
+        assertFalse(entry.hasInlineData());
+        assertEquals(Compression.NONE, entry.getCompression());
+        assertEquals(origSize, entry.getStorageLength());
+
+        // Ok. Then, we should also be able to fetch it, right?
+        response = new FakeHttpResponse();
+        resource.getHandler().getEntry(new FakeHttpRequest(), response, INTERNAL_KEY1);
+        assertEquals(200, response.getStatus());
+
+        // big enough, should be backed by a real file...
+        assertTrue(response.hasStreamingContent());
+        assertTrue(response.hasFile());
+        byte[] data = collectOutput(response);
+
+        // and importantly, should also be auto-uncompressed as expected:
+        assertEquals(origSize, data.length);
+        Assert.assertArrayEquals(INPUT_STR_BYTES, data);
+        
+        entries.stop();
+    }
 }
