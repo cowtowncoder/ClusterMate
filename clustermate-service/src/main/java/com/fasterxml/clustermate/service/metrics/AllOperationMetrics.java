2082840714ecd2443b0318f5e97a69ff47a56757
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-04-10 17:24:52 -0700

    merge GET metrics fix

--- clustermate-service/src/main/java/com/fasterxml/clustermate/service/metrics/AllOperationMetrics.java
+++ clustermate-service/src/main/java/com/fasterxml/clustermate/service/metrics/AllOperationMetrics.java
@@ -2,13 +2,14 @@ package com.fasterxml.clustermate.service.metrics;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({ "GET", "PUT", "DELETE", "LIST", "SYNCLIST", "SYNCPULL" })
+@JsonPropertyOrder({ "GET", "PUT", "DELETE", "INFO", "LIST", "SYNCLIST", "SYNCPULL" })
 public class AllOperationMetrics
 {
     public ExternalOperationMetrics GET;
     public ExternalOperationMetrics PUT;
     public ExternalOperationMetrics DELETE;
 
+    public ExternalOperationMetrics INFO;
     public ExternalOperationMetrics LIST;
 
     public ExternalOperationMetrics SYNCLIST;
