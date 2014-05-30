4cd4302e97efe720471562c838fc4ff36cb0ead1
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-05-29 17:05:47 -0700

    ...

--- clustermate-service/src/main/java/com/fasterxml/clustermate/service/cfg/RemoteClusterConfig.java
+++ clustermate-service/src/main/java/com/fasterxml/clustermate/service/cfg/RemoteClusterConfig.java
@@ -16,13 +16,27 @@ import javax.validation.constraints.*;
  */
 public class RemoteClusterConfig
 {
+    /**
+     * Setting that determines level of redundancy to use for pulling content
+     * from remote cluster(s): between values of 1 and local custer size
+     * (that is, {@link ClusterConfig#numberOfCopies}). Special value
+     * of -1 may be used to mean "use same value as local 'numberOfCopies'".
+     * Choice here is between reliable replication (higher value) and minimal
+     * transfer rates (lowest, that is, 1).
+     * Value itself is used similar to how local node allocation goes, but just
+     * for calculating shared key range portion to request from matching remote
+     * node.
+     */
+    @Min(-1)
+    public int copiesToFetch;
+    
     // // Static configuration of cluster nodes; either complete or partial
     // // since this is only used for boostrapping and actual remote cluster
     // // topology is dynamically requested.
 
     // not mandatory, but if defined, must be valid
-    @Valid
     // also: if defined, must have at least 1 entry
+    @Valid
     @Size(min=1, max=Integer.MAX_VALUE)
     public NodeConfig[] nodes;
 }
